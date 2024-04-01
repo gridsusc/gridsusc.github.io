d65c1cc94887fda9e268f249c2301735a595fcba
Aniket Ladukar <ladukaraniket.projects@gmail.com>
2024-04-01 16:12:31 -0700

    updated about us page

---
+++ src/pages/about-us/members.tsx
@@ -0,0 +1,189 @@
+﻿export const memberJson =
+{
+  "leads": [
+    {
+      "name": "Dr. Saty Raghavachary",
+      "email": "saty@usc.edu",
+      "linkedin": "https://www.linkedin.com/in/satychary/",
+      "photoKey": "saty",
+      "position": "University Advisor"
+    },
+    {
+      "name": "Vatsal Kamlesh Khandor",
+      "email": "khandor@usc.edu",
+      "linkedin": "https://www.linkedin.com/in/khandorvatsal/",
+      "photoKey": "vatsal",
+      "position": "President"
+    }
+  ],
+  "teams": [
+    {
+      "name": "Tech",
+      "vp": [
+        {
+          "name": "Yuko Ohmori",
+          "email": "yohmori@usc.edu",
+          "linkedin": "https://www.linkedin.com/in/yuko-ohmori/",
+          "photoKey": "yuko",
+          "position" : "Vice President"
+        }
+      ],
+      "members": [
+        {
+          "name": "Daksh",
+          "position": "lmao",
+          "email": "dakshhas@usc.edu",
+          "linkedin": "https://www.linkedin.com/in/iamdaksh/",
+          "photoKey": "daksh"
+        },
+        {
+          "name": "Shrey Agarwal",
+          "position": "Member",
+          "email": "shreyaga@usc.edu",
+          "linkedin": "https://www.linkedin.com/in/shrey-agarwal-one1/",
+          "photoKey": "shrey"
+        },
+        {
+          "name": "Anjali Singh",
+          "position": "Member",
+          "email": "singhanj@usc.edu",
+          "linkedin": "https://www.linkedin.com/in/anjalis2112/",
+          "photoKey": "anjali"
+        },
+        {
+          "name": "Ravneet Kaur",
+          "position": "Member",
+          "email": "ravneetk@usc.edu",
+          "linkedin": "https://www.linkedin.com/in/ravneet-kaur78/",
+          "photoKey": "ravneet"
+        }
+      ]
+    },
+    {
+      "name": "Web Development",
+      "vp": [
+        {
+          "name": "Ishaan Singh",
+          "email": "ishaans@usc.edu",
+          "linkedin": "https://linkedin.com/in/bigbrain1901",
+          "photoKey": "ishaan",
+          "position" : "Vice President"
+        }
+      ],
+      "members": [
+        {
+          "name": "Aniket Ladukar",
+          "position": "Member",
+          "email": "ladukar@usc.edu",
+          "linkedin": "https://www.linkedin.com/in/ladukaraniket",
+          "photoKey": "aniket"
+        },
+        {
+          "name": "Devashish Lal",
+          "position": "Member",
+          "email": "devashis@usc.edu",
+          "linkedin": "",
+          "photoKey": "devashish"
+        },
+        {
+          "name": "Chahat",
+          "position": "Member",
+          "email": "chahat@usc.edu",
+          "linkedin": "https://www.linkedin.com/in/chahat7/",
+          "photoKey": "chahat"
+        },
+        {
+          "name": "Miral Gandhi",
+          "position": "Member",
+          "email": "mggandhi@usc.edu",
+          "linkedin": "",
+          "photoKey": "miral"
+        }
+      ]
+    },
+    {
+      "name": "Admin and Sponsorships",
+      "vp": [
+        {
+          "name": "Smrithi Agrawal",
+          "email": "smrithia@usc.edu",
+          "linkedin": "",
+          "photoKey": "smrithi",
+          "position" : "Vice President"
+        }
+      ],
+      "members": [
+        {
+          "name": "Spurthy Skandan",
+          "position": "Member",
+          "email": "skandan@usc.edu",
+          "linkedin": "linkedin.com/in/spurthy-skandan",
+          "photoKey": "spurthy"
+        },
+        {
+          "name": "Ruchit Bhardwaj",
+          "position": "Member",
+          "email": "rrthaker@usc.edu",
+          "linkedin": "",
+          "photoKey": "ruchit"
+        },
+        {
+          "name": "Christopher Bou-Saab",
+          "position": "Member",
+          "email": "bousaab@usc.edu",
+          "linkedin": "https://www.linkedin.com/in/christopher-bousaab/",
+          "photoKey": "christopher"
+        },
+        {
+          "name": "Srinath Begudem",
+          "position": "Member",
+          "email": "begudem@usc.edu",
+          "linkedin": "",
+          "photoKey": "srinath"
+        }
+      ]
+    },
+    {
+      "name": "Outreach & External Affairs",
+      "vp": [
+        {
+          "name": "Shruti Subramanyam",
+          "email": "",
+          "linkedin": "",
+          "photoKey": "shruti",
+          "position" : "Vice President"
+        }
+      ],
+      "members": [
+        {
+          "name": "Kohta",
+          "position": "Member",
+          "email": "",
+          "linkedin": "",
+          "photoKey": "kohta"
+        },
+        {
+          "name": "Satyajit Roy",
+          "position": "Member",
+          "email": "ssroy@usc.edu",
+          "linkedin": "https://www.linkedin.com/in/satyajit-roy060801/",
+          "photoKey": "satyajit"
+        },
+        {
+          "name": "Mandy Chen",
+          "position": "Member",
+          "email": "mandyche@usc.edu",
+          "linkedin": "http://linkedin.com/in/mandymanni-chen",
+          "photoKey": "mandy"
+        },
+        {
+          "name": "Ekaterina Shtyrkova",
+          "position": "Member",
+          "email": "shtyrkov@usc.edu",
+          "linkedin": "",
+          "photoKey": "ekaterina"
+        }
+      ]
+    }
+  ]
+}
